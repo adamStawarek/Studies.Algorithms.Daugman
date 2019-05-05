bb193046928fb63fe12d94c05572e1836fc89281
adam stawarek <adam.stawarek@op.pl>
2019-05-05 21:44:33 +0200

    histogram equalization+floodFill

--- ImageEditor/Filters/Daugman.cs
+++ ImageEditor/Filters/Daugman.cs
@@ -10,18 +10,127 @@ namespace ImageEditor.Filters
     public class Daugman : IFilter
     {
         public string Name => "Daugman";
-        private int _maxRadius = 60;
+        private readonly int _maxRadius = 80;
+        private readonly int _minRadius = 30;
         private Bitmap _processedBitmap;
 
         public Bitmap Filter(Bitmap bitmap)
         {
+
             _processedBitmap = bitmap;
+            var pn = GetProbabilities();
+            ApplyHistogramEqualization(pn);
+            Point startPoint = FindFloodFillStaringPoint();
+            ApplyFloodFill(startPoint);
             var thresholdPixels = ApplyThersholding();
             var localMinPixels = FindLocalMinimums(ref thresholdPixels);
             var pointMaxIntensityDifferences = CalculateCircularPixelsIntensities(localMinPixels);
             MarkObtainedPoints(pointMaxIntensityDifferences);
-
+            _processedBitmap.SetPixel(startPoint.X, startPoint.Y, Color.Pink);
+            _processedBitmap.SetPixel(startPoint.X - 1, startPoint.Y, Color.Pink);
+            _processedBitmap.SetPixel(startPoint.X + 1, startPoint.Y, Color.Pink);
+            _processedBitmap.SetPixel(startPoint.X - 1, startPoint.Y + 1, Color.Pink);
+            _processedBitmap.SetPixel(startPoint.X, startPoint.Y + 1, Color.Pink);
+            _processedBitmap.SetPixel(startPoint.X + 1, startPoint.Y + 1, Color.Pink);
+            _processedBitmap.SetPixel(startPoint.X - 1, startPoint.Y - 1, Color.Pink);
+            _processedBitmap.SetPixel(startPoint.X, startPoint.Y - 1, Color.Pink);
+            _processedBitmap.SetPixel(startPoint.X + 1, startPoint.Y - 1, Color.Pink);
             return _processedBitmap;
+        }
+      
+        private double[] GetProbabilities()
+        {
+            var intensityCount = new Dictionary<byte,int>();
+            for (int i = 0; i <= 255; i++)
+            {
+                intensityCount.Add((byte)i,0);
+            }
+            for (int y = 0; y < _processedBitmap.Height; y++)
+            {
+                for (int x = 0; x < _processedBitmap.Width; x++)
+                {
+                    var pixel = _processedBitmap.GetPixel(x, y);
+                    byte blue = pixel.R;
+                    byte green = pixel.G;
+                    byte red = pixel.B;
+                    var grayScale = GetGreyscale(red, green, blue);
+                    intensityCount[grayScale]++;
+                }
+            }
+
+            var totalPixelCount = _processedBitmap.Width * _processedBitmap.Height;
+            var pn = new Double[256];
+            foreach (var i in intensityCount)
+            {
+                pn[i.Key] = i.Value / (double)totalPixelCount;
+            }
+
+            return pn;
+        }
+
+        private void ApplyHistogramEqualization(double[] pn)
+        {
+            for (int y = 0; y < _processedBitmap.Height; y++)
+            {
+                for (int x = 0; x < _processedBitmap.Width; x++)
+                {
+                    var pixel = _processedBitmap.GetPixel(x, y);
+                    byte blue = pixel.R;
+                    byte green = pixel.G;
+                    byte red = pixel.B;
+                    var grayScale = GetGreyscale(red, green, blue);
+                    var newIntensity = (byte)Math.Floor(255 * pn.Take(grayScale).Sum());
+                    _processedBitmap.SetPixel(x,y,Color.FromArgb(newIntensity,newIntensity,newIntensity));
+                }
+            }
+        }
+
+        private Point FindFloodFillStaringPoint()
+        {
+            var offsetWidth = _processedBitmap.Width / 3;
+            var offsetHeight = _processedBitmap.Height / 3;
+            (Point? point, byte intensity) point= (null, 0);
+            for (int y = offsetHeight; y < _processedBitmap.Height-offsetHeight; y++)
+            {
+                for (int x = offsetWidth; x < _processedBitmap.Width-offsetWidth; x++)
+                {
+                    var pixel = _processedBitmap.GetPixel(x, y);
+                    byte blue = pixel.R;
+                    byte green = pixel.G;
+                    byte red = pixel.B;
+                    var grayScale = GetGreyscale(red, green, blue);
+                    if (point.point == null || point.intensity < grayScale)
+                        point = (new Point(x, y), grayScale);
+                }
+            }
+
+            return (Point)point.point;
+        }
+
+        private void ApplyFloodFill(Point startPoint)
+        {
+            Stack<Point> pixels = new Stack<Point>();
+            var targetColor = _processedBitmap.GetPixel(startPoint.X, startPoint.Y);
+            var targetIntensity = GetGreyscale(targetColor.R, targetColor.G, targetColor.B);
+            pixels.Push(startPoint);
+            var offset = 40;
+            while (pixels.Count > 0)
+            {
+                Point a = pixels.Pop();
+                if (a.X < _processedBitmap.Width && a.X > -1 && a.Y < _processedBitmap.Height && a.Y > -1)
+                {
+                    var pixelColor = _processedBitmap.GetPixel(a.X, a.Y);
+                    var pixelIntensity = GetGreyscale(pixelColor.R, pixelColor.G, pixelColor.B);
+                    if (pixelIntensity >= targetIntensity-offset&& pixelIntensity <= targetIntensity + offset)
+                    {
+                        _processedBitmap.SetPixel(a.X, a.Y, Color.Black);
+                        pixels.Push(new Point(a.X - 1, a.Y));
+                        pixels.Push(new Point(a.X + 1, a.Y));
+                        pixels.Push(new Point(a.X, a.Y - 1));
+                        pixels.Push(new Point(a.X, a.Y + 1));
+                    }
+                }
+            }
         }
 
         private List<Point> ApplyThersholding()
@@ -76,10 +185,10 @@ namespace ImageEditor.Filters
             }
 
             //remove points that are closer that _minRadius to image edges
-            localMinPixels = localMinPixels.Where(p => p.X + _maxRadius < _processedBitmap.Width
-                                                       && p.X - _maxRadius > 0
-                                                       && p.Y + _maxRadius < _processedBitmap.Height
-                                                       && p.Y - _maxRadius > 0).ToList();
+            //localMinPixels = localMinPixels.Where(p => p.X + _maxRadius < _processedBitmap.Width
+            //                                           && p.X - _maxRadius > 0
+            //                                           && p.Y + _maxRadius < _processedBitmap.Height
+            //                                           && p.Y - _maxRadius > 0).ToList();
 
             return localMinPixels;
         }       
@@ -93,7 +202,19 @@ namespace ImageEditor.Filters
             {
                 var previousIntensities = 0;
                 pointMaxIntensityDifferences.Add(point, new CircleIntensity());
-                for (int i = 25; i < _maxRadius; i++)
+
+                var distancesToBitmapEdges=new List<int>
+                {
+                    _processedBitmap.Height - point.Y,
+                    point.Y,
+                    point.X,
+                    _processedBitmap.Width-point.X
+                };
+                var maxPointRadius = distancesToBitmapEdges.Min();
+                if(maxPointRadius<_minRadius) continue;
+
+
+                for (int i = _minRadius; i <  maxPointRadius; i++)
                 {
                     var circularPoints = point.GetCircularPoints(i, Math.PI / 6.0f);
 
@@ -122,9 +243,10 @@ namespace ImageEditor.Filters
 
         private void MarkObtainedPoints(Dictionary<Point, CircleIntensity> pointMaxIntensityDifferences)
         {
+
             var point2 = pointMaxIntensityDifferences.OrderByDescending(p => p.Value.DiffIntensity).First();
             LogService.Write($"Selected center: {point2.Key.X} , {point2.Key.Y} ,radius: {point2.Value.Radius} , " +
-                             $"diffIntensity: {point2.Value.DiffIntensity}");
+                             $"DiffIntensity: {point2.Value.DiffIntensity}");
 
 
             _processedBitmap.SetPixel(point2.Key.X, point2.Key.Y, Color.Yellow);
@@ -148,6 +270,36 @@ namespace ImageEditor.Filters
                 _processedBitmap.SetPixel(p.X, p.Y - 1, Color.Red);
                 _processedBitmap.SetPixel(p.X + 1, p.Y - 1, Color.Red);
             }
+
+            //var points = pointMaxIntensityDifferences.OrderByDescending(p => p.Value.DiffIntensity).Take(10).ToList();
+            ////LogService.Write($"Selected center: {point2.Key.X} , {point2.Key.Y} ,radius: {point2.Value.Radius} , " +
+            ////                 $"diffIntensity: {point2.Value.DiffIntensity}");
+
+
+            //foreach (var point2 in points)
+            //{
+            //    _processedBitmap.SetPixel(point2.Key.X, point2.Key.Y, Color.Yellow);
+            //    _processedBitmap.SetPixel(point2.Key.X - 1, point2.Key.Y, Color.Yellow);
+            //    _processedBitmap.SetPixel(point2.Key.X + 1, point2.Key.Y, Color.Yellow);
+            //    _processedBitmap.SetPixel(point2.Key.X - 1, point2.Key.Y + 1, Color.Yellow);
+            //    _processedBitmap.SetPixel(point2.Key.X, point2.Key.Y + 1, Color.Yellow);
+            //    _processedBitmap.SetPixel(point2.Key.X + 1, point2.Key.Y + 1, Color.Yellow);
+            //    _processedBitmap.SetPixel(point2.Key.X - 1, point2.Key.Y - 1, Color.Yellow);
+            //    _processedBitmap.SetPixel(point2.Key.X, point2.Key.Y - 1, Color.Yellow);
+            //    _processedBitmap.SetPixel(point2.Key.X + 1, point2.Key.Y - 1, Color.Yellow); 
+            //}
+            //foreach (var p in points[0].Key.GetCircularPoints(points[0].Value.Radius, Math.PI / 6.0f))
+            //{
+            //    _processedBitmap.SetPixel(p.X, p.Y, Color.Red);
+            //    _processedBitmap.SetPixel(p.X - 1, p.Y, Color.Red);
+            //    _processedBitmap.SetPixel(p.X + 1, p.Y, Color.Red);
+            //    _processedBitmap.SetPixel(p.X - 1, p.Y + 1, Color.Red);
+            //    _processedBitmap.SetPixel(p.X, p.Y + 1, Color.Red);
+            //    _processedBitmap.SetPixel(p.X + 1, p.Y + 1, Color.Red);
+            //    _processedBitmap.SetPixel(p.X - 1, p.Y - 1, Color.Red);
+            //    _processedBitmap.SetPixel(p.X, p.Y - 1, Color.Red);
+            //    _processedBitmap.SetPixel(p.X + 1, p.Y - 1, Color.Red);
+            //}
         }
 
         private byte GetGreyscale(byte r, byte g, byte b)
